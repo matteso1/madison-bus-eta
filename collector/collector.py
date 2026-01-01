5a1a68dacea52baffb4c8fb3facf6e04945afb76
Nils Matteson <nilsmatteson@icloud.com>
2025-12-31 20:20:08 -0700

    fix: remove syntax error in collector.py

--- collector/collector.py
+++ collector/collector.py
@@ -68,7 +68,7 @@ logging.basicConfig(
     format='%(asctime)s | %(levelname)s | %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
-)
+
 logger = logging.getLogger(__name__)
 
 # Sentinel Client Instance
